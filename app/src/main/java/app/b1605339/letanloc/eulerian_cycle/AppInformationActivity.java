d254093e5d2cd3cea2c6b7d4a2a59d225fbe3304
letanloc-cloud <46790464+letanloc-cloud@users.noreply.github.com>
2019-01-18 01:31:20 +0700

    Create
    Create Eulerian_Cycle
    letanloc.b1605339.app.eulerian_cycle

---
+++ app/src/main/java/app/b1605339/letanloc/eulerian_cycle/AppInformationActivity.java
@@ -0,0 +1,13 @@
+package app.b1605339.letanloc.eulerian_cycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_information);
+    }
+}
