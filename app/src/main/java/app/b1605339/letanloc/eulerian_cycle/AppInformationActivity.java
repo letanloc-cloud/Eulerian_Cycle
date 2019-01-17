694843c5cb2f642652892ef8045ec15b5364f7f4
letanloc-cloud <46790464+letanloc-cloud@users.noreply.github.com>
2019-01-18 01:56:50 +0700

    Create Activity
    Create BeginActivity
    Design BeginActivity
    Open BeginActivity when btnStart is clicked

--- app/src/main/java/app/b1605339/letanloc/eulerian_cycle/AppInformationActivity.java
+++ app/src/main/java/app/b1605339/letanloc/eulerian_cycle/AppInformationActivity.java
@@ -1,7 +1,10 @@
 package app.b1605339.letanloc.eulerian_cycle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AppInformationActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class AppInformationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_information);
+
+        Button btnStart = (Button) findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AppInformationActivity.this, BeginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/app/b1605339/letanloc/eulerian_cycle/BeginActivity.java
@@ -0,0 +1,13 @@
+package app.b1605339.letanloc.eulerian_cycle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BeginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_begin);
+    }
+}
